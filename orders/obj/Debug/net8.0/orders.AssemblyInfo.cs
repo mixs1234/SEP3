4de64b0769daa4a32b91611da9e1458ac2fd9b4d
mixs1234 <mixs12344@gmail.com>
2024-11-06 08:35:44 +0100

    rendermode removed

--- orders/obj/Debug/net8.0/orders.AssemblyInfo.cs
+++ orders/obj/Debug/net8.0/orders.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("orders")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08b6d85c6de2743fcccbf35ef465d0ceea2bc705")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eabd3dbec2965841acee72df4d57c84242aaabc")]
 [assembly: System.Reflection.AssemblyProductAttribute("orders")]
 [assembly: System.Reflection.AssemblyTitleAttribute("orders")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
