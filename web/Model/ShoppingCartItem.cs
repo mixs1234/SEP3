f910a39de181a939f89699cf5474cd83e6efd516
mikkel077 <mikkelhansen077@gmail.com>
2024-12-03 10:05:57 +0100

    To Commit

---
+++ web/Model/ShoppingCartItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace web.Model;
+
+public class ShoppingCartItem
+{
+    public long VariantId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public string Size { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ web/Services/ShoppingCartService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using web.Model;
+
+namespace web.Services;
+
+public class ShoppingCartService
+{
+    private readonly List<ShoppingCartItem> _productVariants = new List<ShoppingCartItem>();
+
+
+    public void AddToCart(ShoppingCartItem shoppingCartItem)
+    {
+        _productVariants.Add(shoppingCartItem);
+    }
+
+    public void RemoveFromCart(ShoppingCartItem shoppingCartItem)
+    {
+        _productVariants.Remove(shoppingCartItem);
+    }
+}
