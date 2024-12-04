7699f2f92a741f2d115eba6510ead5452a48ef2f
Victor Meldgaard <49498101+meldg0d@users.noreply.github.com>
2024-12-04 02:22:53 +0100

    fixed syntax

--- web/Model/OrderResponse.cs
+++ web/Model/OrderResponse.cs
@@ -8,21 +8,21 @@ public class OrderResponse
     public OrderResponse(bool isSuccess, string orderId, HttpStatusCode statusCode)
     {
         IsSuccess = isSuccess;
-        this.orderId = orderId;
+        this.OrderId = orderId;
         StatusCode = statusCode;
     }
 
     public OrderResponse(bool isSuccess, string orderId, string? errorMessage, HttpStatusCode statusCode)
     {
         IsSuccess = isSuccess;
-        this.orderId = orderId;
+        this.OrderId = orderId;
         ErrorMessage = errorMessage;
         StatusCode = statusCode;
     }
 
 
     public bool IsSuccess { get; set; }
-    public string orderId { get; set; }
+    public string OrderId { get; set; }
     public string? ErrorMessage { get; set; }
     public System.Net.HttpStatusCode StatusCode { get; set; }
 }
