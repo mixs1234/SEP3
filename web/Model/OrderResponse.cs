63037985ba5e4e7d30034fba39afe7ef0dd3a101
Victor Meldgaard <49498101+meldg0d@users.noreply.github.com>
2024-12-05 19:29:19 +0100

    notificaitons added and fixed small async problem

---
+++ web/Model/OrderResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using web.Model;
+
+namespace sep3.broker.Model;
+
+public class OrderResponse
+{
+    public OrderResponse(bool isSuccess, string orderId, HttpStatusCode statusCode)
+    {
+        IsSuccess = isSuccess;
+        this.OrderId = orderId;
+        StatusCode = statusCode;
+    }
+
+    public OrderResponse(bool isSuccess, string orderId, string? errorMessage, HttpStatusCode statusCode)
+    {
+        IsSuccess = isSuccess;
+        this.OrderId = orderId;
+        ErrorMessage = errorMessage;
+        StatusCode = statusCode;
+    }
+
+
+    public bool IsSuccess { get; set; }
+    public string OrderId { get; set; }
+    public string? ErrorMessage { get; set; }
+    public System.Net.HttpStatusCode StatusCode { get; set; }
+}
