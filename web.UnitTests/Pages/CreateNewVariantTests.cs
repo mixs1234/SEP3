35773f6bf61560a71b6cc07296d13caaefa7adf3
Kyrnu <Kyrnu@users.noreply.github.com>
2024-12-06 08:52:45 +0100

    web: Test project setup

---
+++ web.UnitTests/Pages/CreateNewVariantTests.cs
@@ -0,0 +1,21 @@
+using sep3.DTO;
+using sep3.web;
+//using sep3.web.UnitTests.Mocks;
+using Bunit;
+
+namespace sep3.web.UnitTests.Pages;
+
+public class CreateNewVariantTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void TestNewVariant()
+    {
+        Assert.Pass();
+    }
+}
