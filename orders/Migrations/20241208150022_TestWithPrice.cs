6eaca562f4770c608566f425dde3393cb94b65fa
Victor Meldgaard <49498101+meldg0d@users.noreply.github.com>
2024-12-08 17:46:21 +0100

    init

---
+++ orders/Migrations/20241208150022_TestWithPrice.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace orders.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestWithPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Materials",
+                table: "CartItems",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "CartItems",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductName",
+                table: "CartItems",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Size",
+                table: "CartItems",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Materials",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "ProductName",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "CartItems");
+        }
+    }
+}
